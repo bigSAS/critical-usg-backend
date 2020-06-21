b45e50acf9d378314f57816311a58f589abcaa22
bigSAS <0980_GitHub>
2020-06-21 11:56:44 +0200

    postgres wip

--- db/model.py
+++ db/model.py
@@ -5,7 +5,7 @@ from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, DateTime
 from sqlalchemy.ext.hybrid import hybrid_property, hybrid_method
-
+from sqlalchemy.types import JSON
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
@@ -19,6 +19,7 @@ class User(db.Model):
     _password = db.Column(db.LargeBinary(100))
     is_superuser = db.Column(db.Boolean, default=False)
     groups = db.relationship('GroupUser', backref='user', lazy=True)
+    is_deleted = db.Column(db.Boolean, default=False)
 
     def __init__(self, email: str,  plaintext_password: str, username: str = None, is_superuser: bool = False):
         self.email = email
@@ -43,17 +44,24 @@ class User(db.Model):
         group_ids = set([gu.group_id for gu in self.groups])
         return UserGroup.query.filter(UserGroup.id.in_(group_ids)).all()
 
+    def delete(self):
+        self.is_deleted = True
+
     def as_dict(self):
         return {
             'id': self.id,
             'email': self.email,
             'username': self.username,
             'is_superuser': self.is_superuser,
-            'groups': [ug.name for ug in self.user_groups]
+            'groups': [ug.name for ug in self.user_groups],
+            'is_deleted': self.is_deleted
         }
 
-    def __repr__(self):
+    def __str__(self):
         return f'User{self.as_dict()}'
+
+    def __repr__(self):
+        return self.__str__()
 
 
 class UserGroup(db.Model):
@@ -132,19 +140,20 @@ class InstructionDocumentPage(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     document_id = db.Column(db.Integer, db.ForeignKey('instruction_document.id'), nullable=False)
     page_num = db.Column(db.Integer, default=0)
-    json_data = db.Column(db.Text(500_000), nullable=True)
+    json = db.Column(JSON, nullable=True)
 
-    @property
-    def data(self):
-        return loads(self.json_data) if self.json_data else None
+    # @property
+    # def data(self):
+    #     return loads(self.json_data) if self.json_data else None
 
     def as_dict(self):
         return {
             'id': self.id,
             'document_id': str(self.document_id),
             'page_num': self.page_num,
+            'json': self.json,
             # 'json_data': self.json_data,  # todo: uncomment for debug
-            'data': self.data
+            # 'data': self.data
         }
 
     def __repr__(self):
