db9cac6d242dc6f5b8235f461bafa4486f53912b
bigSAS <0980_GitHub>
2020-07-03 18:34:37 +0200

    .

--- blueprints/instruction_document.py
+++ blueprints/instruction_document.py
@@ -12,7 +12,6 @@ def handle_error(error: Exception):
     return error_response(error)
 
 
-# noinspection PyTypeChecker
 @instruction_document_blueprint.route('/add-doc', methods=('POST',))
 @restricted(['ADMIN'])
 def add_doc():
@@ -20,7 +19,6 @@ def add_doc():
     return event_handler_for(request).get_response()
 
 
-# noinspection PyTypeChecker
 @instruction_document_blueprint.route('/delete-doc', methods=('POST',))
 @restricted(['ADMIN'])
 def delete_doc():
@@ -28,7 +26,6 @@ def delete_doc():
     return event_handler_for(request).get_response()
 
 
-# noinspection PyTypeChecker
 @instruction_document_blueprint.route('/update-doc', methods=('POST',))
 @restricted(['ADMIN'])
 def update_doc():
@@ -36,7 +33,6 @@ def update_doc():
     return event_handler_for(request).get_response()
 
 
-# noinspection PyTypeChecker
 @instruction_document_blueprint.route('/add-page', methods=('POST',))
 @restricted(['ADMIN'])
 def add_page():
@@ -44,7 +40,6 @@ def add_page():
     return event_handler_for(request).get_response()
 
 
-# noinspection PyTypeChecker
 @instruction_document_blueprint.route('/update-page', methods=('POST',))
 @restricted(['ADMIN'])
 def update_page():
@@ -52,7 +47,6 @@ def update_page():
     return event_handler_for(request).get_response()
 
 
-# noinspection PyTypeChecker
 @instruction_document_blueprint.route('/delete-page', methods=('POST',))
 @restricted(['ADMIN'])
 def delete_page():
