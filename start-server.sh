5a909db2ef8d46c19e3035bb7332ed22146e7613
bigSAS <0980_GitHub>
2020-07-12 15:59:06 +0200

    db defaults bugfix

--- start-server.sh
+++ start-server.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-(cd /opt/app; pipenv run flask db upgrade; pipenv run gunicorn --bind 0.0.0.0:8000 --workers $GUNICORN_WORKERS wsgi:app) &
+(cd /opt/app; pipenv run flask db upgrade; pipenv python setup_defaults.py; pipenv run gunicorn --bind 0.0.0.0:8000 --workers $GUNICORN_WORKERS wsgi:app) &
 nginx -g "daemon off;"
