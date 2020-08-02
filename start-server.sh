1aeddcc48c90571f69fd0495a693bec6a5e25f98
bigSAS <sas.it.tomasz.majk@gmail.com>
2020-08-02 21:20:03 +0200

    update img fix sh

--- start-server.sh
+++ start-server.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
-(cd /opt/app; pipenv run flask db upgrade; pipenv run python setup_defaults.py; pipenv run gunicorn --bind 0.0.0.0:8000 --workers $GUNICORN_WORKERS wsgi:app) &
+(
+cd /opt/app;
+flask db upgrade;
+python setup_defaults.py;
+gunicorn --bind 0.0.0.0:8000 --workers $GUNICORN_WORKERS wsgi:app
+) &
 nginx -g "daemon off;"
