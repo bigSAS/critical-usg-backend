20c6894670c74ac5dc72b358299e39b95e52e494
bigSAS <sas.it.tomasz.majk@gmail.com>
2020-09-10 22:18:44 +0200

    update sh .

--- start-server.sh
+++ start-server.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
-(
-cd /opt/app;
-python manage.py db upgrade;
-gunicorn --bind 0.0.0.0:8000 --workers $CUSG_GUNICORN_WORKERS "cusg:create_app()"
-) &
-nginx -g "daemon off;"
+cd /opt/app \
+&& python manage.py db upgrade \
+&& gunicorn --bind 0.0.0.0:8000 --workers $CUSG_GUNICORN_WORKERS "cusg:create_app()" \
+&& nginx -g "daemon off;"
